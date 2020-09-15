0d272a7c046c9c51ff0b6c0c419c4676e67a52ed
lgiacalo <lgiacalo@student.42.fr>
2020-09-15 20:56:03 +0200

    edit assert

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -6,6 +6,6 @@ var assert = require('assert');
 assert.strictEqual(typeof(capitalizeFirstLetters), 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD '), 'I Am Learning TDD');
-assert.strictEqual(capitalizeFirstLetters('learning'), 'Learning');
-assert.strictEqual(capitalizeFirstLetters(''), '');
+assert.deepStrictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+assert.deepStrictEqual(capitalizeFirstLetters('learning'), 'Learning');
+assert.deepStrictEqual(capitalizeFirstLetters(''), '');
