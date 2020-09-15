1c370da072c69202a887538f5da15fbf13d18180
lgiacalo <lgiacalo@student.42.fr>
2020-09-15 21:45:16 +0200

    code method class Rectangle

--- Rectangle.js
+++ Rectangle.js
@@ -6,15 +6,15 @@ class Rectangle {
     }
   
     isSquare() {
-  
+        return this.a === this.b
     }
   
     getArea() {
-  
+        return this.a * this.b
     }
   
     getPerimeter() {
-  
+        return this.a * 2 + this.b * 2
     }
   }
   
